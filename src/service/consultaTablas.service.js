ba132a3eccefca3b3780f38e8dda109c012a6bcd
CarlosMarquez10 <carlos27marquez10@gmail.com>
2026-02-26 11:21:43 -0500

    se agrega servicio para consultar la talba de revisiones de sirius.

--- src/service/consultaTablas.service.js
+++ src/service/consultaTablas.service.js
@@ -2,21 +2,28 @@ import {pool} from '../Connetion/db.js';
 
 export async function TablaCorreria () {
   try {
-    const [rows] = await pool.execute('SELECT * FROM correrias');
+    const [rows] = await pool.execute('SELECT correria FROM correrias');
     if (!rows || rows.length === 0) return null;
-
-    const data = rows.map((e) => ({
-      correria: e.Correia 
-    }));
-
-    return data; // <-- los datos para compartir o manejar en otro modulos
+    return rows; // <-- los datos para compartir o manejar en otro modulos
   } catch (error) {
     console.error('Error en TablaCorreria:', error);
     throw error; // para que quien llame pueda manejarlo
   }
 }
 
-// Luego:
-TablaCorreria()
-  .then((data) => console.log(data))
-  .catch(console.error);
+
+// tabla de revisiones Sirius
+
+export async function TablaRevisionesSirius () {
+    try {
+        const [rows] = await pool.execute('SELECT * FROM revisiones_sirius');
+        if (!rows || rows.length === 0) return null;
+        console.log('Datos de TablaRevisionesSirius:', rows); // <-- para verificar los datos obtenidos
+        return rows; // <-- los datos para compartir o manejar en otro modulos
+    } catch (error) {
+        console.error('Error en TablaRevisionesSirius:', error);
+        throw error; // para que quien llame pueda manejarlo
+    }
+}
+
+TablaRevisionesSirius();
