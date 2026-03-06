7c207cd30e49d9e0578f6ec754f032b93c6d9be9
CarlosMarquez10 <carlos27marquez10@gmail.com>
2026-03-06 05:44:07 -0500

    se agrega lo de gpstable.service para recuperar los datos.

--- src/service/gpstable.service.js
+++ src/service/gpstable.service.js
@@ -2,11 +2,35 @@ import { pool } from "../Connetion/db.js";
 
 export async function GetCars() {
   try {
+
+    const tipoCars = ["HSX795",
+      "GJU140",
+      "GCU826",
+      "CWZ891",
+      "MGY346",
+      "JZM050",
+      "CUX039",
+      "HPW184"
+,"LTO423"];   
+
     const [rows] = await pool.execute(
       "SELECT * FROM cars",
     );
+    // recorrer el array de resultados y mostrar algunas propiedaddes con map
+
+        const cars = rows.map(car => ({
+          alias: car.alias,
+          longitude: car.longitude,
+          latitude: car.latitude,
+          speed: car.speed,
+          active: car.active,
+          marker_color: car.active === "ONLINE" ? car.marker_color : "#FF2C2C",
+          type: tipoCars.includes(car.alias) ? "carro" : "moto" // Asigna el tipo basado en el alias
+        }));
+
+
    // console.log(rows[0]);
-    return rows.length > 0 ? rows : null;
+    return cars.length > 0 ? cars : null;
   } catch (error) {
     console.error(`Error al consultar correria:`, error);
     return null;
